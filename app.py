eb14a070c3168f62e0c8a948e86daf38d3de2c8b
rpgzeflorentino <130254239+rpgzeflorentino@users.noreply.github.com>
2023-04-08 19:30:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,18 @@ def ficha():
 
 @app.route('/ficha', methods=['POST'])
 def resultado():
-  nome = str(request.form['nome'])
-  origem = str(request.form['origem'])
-  coman = str(request.form['coman'])
-  cie = str(request.form['cie'])
-  manu = str(request.form['manu'])
-  seg = str(request.form['seg'])
-  est = str(request.form['est'])
-  equip = str(request.form['equip'])
+  varnome = str(request.form['nome'])
+  varorigem = str(request.form['origem'])
+  varcoman = str(request.form['coman'])
+  varcie = str(request.form['cie'])
+  varmanu = str(request.form['manu'])
+  varseg = str(request.form['seg'])
+  varequip = str(request.form['equip'])
   return render_template('ficha.html',
-                         nome = nome,
-                         origem = origem,
-                         max_com = coman,
-                         max_manu = manu,
-                         max_cie = cie,
-                         max_sec = seg,
-                         max_est = est,
+                         nome = varnome,
+                         origem = varorigem,
+                         max_com = varcoman,
+                         max_manu = varmanu,
+                         max_cie = varcie,
+                         max_sec = varseg,
                          equipamentos = equip)
